d53aba2fcf9a91f1a48baf6dd68e5c40039dbc29
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-05-16 16:28:24 +0530

    initial setup of the server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+
+app.use("/text",(req,res)=>{
+    res.send("world");
+})
+
+app.use((req,res)=>{
+    res.send("hello world");
+})
+
+
+app.listen(3000,()=>{
+    console.log("listning on port 3000");
+})
