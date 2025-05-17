4e27249ee3dd9d97e0b549d93171de92051af969
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-05-17 12:56:15 +0530

    experiment with different route handlers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express=require("express");
 const app=express();
 
-app.use("/text",(req,res)=>{
-    res.send("hey world");
-})
 
+app.use("/text",(req,res,next)=>{
+    console.log("bleh");
+    res.send("hey world");
+    next();
+}, (req,res)=>{
+    console.log("blah");
+res.send("hey2 world");
+});
+app.use("/abc/:id",(req,res)=>{
+    console.log(req.params);
+    res.send("aaru");
+})
 app.use((req,res)=>{
     res.send("hello world");
 })
